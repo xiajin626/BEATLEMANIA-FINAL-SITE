3d56b2ad87b054524dda555d24197b2eafaccbbe
xiajin626 <31479840+xiajin626@users.noreply.github.com>
2018-05-05 11:35:51 -0500

    Add files via upload

---
+++ script-home.js
@@ -0,0 +1,91 @@
+$( document ).ready(function() {
+    $( ".crying" ).click(function() {
+  $( ".overlay2" ).fadeTo( "slow", 1 ); 
+  $( ".overlay7" ).fadeTo( "slow", 1 );
+  $( ".overlay8" ).fadeTo( "slow", 1 );
+  $( ".overlay10" ).fadeTo( "slow", 1 );
+  $( ".overlay13" ).fadeTo( "slow", 1 );
+  $( ".overlay16" ).fadeTo( "slow", 1 );
+  $( ".overlay20" ).fadeTo( "slow", 1 );
+  $( ".overlay21" ).fadeTo( "slow", 1 );
+  $( ".overlay23" ).fadeTo( "slow", 1 );
+  $( ".overlay32" ).fadeTo( "slow", 1 );
+  $( ".overlay38" ).fadeTo( "slow", 1 );
+  $( ".overlay41" ).fadeTo( "slow", 1 );
+  $( ".overlay42" ).fadeTo( "slow", 1 );
+  $( ".overlay44" ).fadeTo( "slow", 1 );
+  $( ".overlay50" ).fadeTo( "slow", 1 );
+  $( ".overlay52" ).fadeTo( "slow", 1 );
+  $( ".overlay53" ).fadeTo( "slow", 1 );
+  $( ".base" ).fadeTo( "slow", 0 );
+});
+});
+
+$( document ).ready(function() {
+    $( ".fainting" ).click(function() {
+  $( ".overlay1" ).fadeTo( "slow", 1 );
+  $( ".overlay4" ).fadeTo( "slow", 1 ); 
+  $( ".overlay54" ).fadeTo( "slow", 1 ); 
+  $( ".base" ).fadeTo( "slow", 0 );
+});
+});
+
+$( document ).ready(function() {
+    $( ".question" ).click(function() {
+  $( ".overlay5" ).fadeTo( "slow", 1 );
+  $( ".overlay14" ).fadeTo( "slow", 1 );
+  $( ".overlay22" ).fadeTo( "slow", 1 );
+  $( ".overlay24" ).fadeTo( "slow", 1 );
+  $( ".overlay25" ).fadeTo( "slow", 1 );
+  $( ".overlay26" ).fadeTo( "slow", 1 );
+  $( ".overlay29" ).fadeTo( "slow", 1 );
+  $( ".overlay31" ).fadeTo( "slow", 1 );
+  $( ".overlay37" ).fadeTo( "slow", 1 );
+  $( ".overlay45" ).fadeTo( "slow", 1 );
+  $( ".overlay57" ).fadeTo( "slow", 1 );
+  $( ".base" ).fadeTo( "slow", 0 );
+});
+});
+
+$( document ).ready(function() {
+    $( ".screaming" ).click(function() {
+  $( ".overlay9" ).fadeTo( "slow", 1 );
+  $( ".overlay17" ).fadeTo( "slow", 1 );
+  $( ".overlay19" ).fadeTo( "slow", 1 );
+  $( ".overlay27" ).fadeTo( "slow", 1 );
+  $( ".overlay28" ).fadeTo( "slow", 1 );
+  $( ".overlay30" ).fadeTo( "slow", 1 );
+  $( ".overlay33" ).fadeTo( "slow", 1 );
+  $( ".overlay35" ).fadeTo( "slow", 1 );
+  $( ".overlay39" ).fadeTo( "slow", 1 );
+  $( ".overlay40" ).fadeTo( "slow", 1 );
+  $( ".overlay43" ).fadeTo( "slow", 1 );
+  $( ".overlay46" ).fadeTo( "slow", 1 );
+  $( ".overlay47" ).fadeTo( "slow", 1 );
+  $( ".overlay51" ).fadeTo( "slow", 1 );
+  $( ".overlay58" ).fadeTo( "slow", 1 );
+  $( ".overlay59" ).fadeTo( "slow", 1 );
+  $( ".base" ).fadeTo( "slow", 0 );
+});
+});
+
+$( document ).ready(function() {
+    $( ".calming" ).click(function() {
+  $( ".overlay15" ).fadeTo( "slow", 1 );
+  $( ".overlay18" ).fadeTo( "slow", 1 );
+  $( ".overlay34" ).fadeTo( "slow", 1 );
+  $( ".overlay36" ).fadeTo( "slow", 1 );
+  $( ".overlay49" ).fadeTo( "slow", 1 );
+  $( ".overlay55" ).fadeTo( "slow", 1 );
+  $( ".overlay56" ).fadeTo( "slow", 1 );
+  $( ".overlay60" ).fadeTo( "slow", 1 );
+  $( ".base" ).fadeTo( "slow", 0 );
+});
+});
+
+$( document ).ready(function() {
+  $( ".kissing" ).click(function() {
+  $( ".overlay48" ).fadeTo( "slow", 1 ); 
+  $( ".base" ).fadeTo( "slow", 0 );
+});
+});
